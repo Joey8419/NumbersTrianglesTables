1edebd5d621d4c3fa1385da0fd95cc22fc29557d
Joey <joey@Zipcoders-MacBook-Pro-9.local>
2023-10-29 19:17:14 -0400

    tried Number Utilities first

--- src/main/java/io/zipcoder/microlabs/mastering_loops/MainApp.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/MainApp.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 /**
  * Created by leon on 1/31/18.
  */
-public class MainApp {
+public class  MainApp {
     public static void main(String[] args) {
     }
 }
--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,34 +3,77 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        int number = start;
+        while (number < stop) {
+            result += number;
+            number += 2;
+        }
+        return result;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        int number = start;
+        while (number < stop) {
+            result += number;
+            result += 3;
+        }
+
+        return result;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String result = "";
+        int number = start;
+        while (number < stop) {
+            number += 1;
+            number = (int)Math.sqrt(number);
+        }
+        return result;
     }
 
     public static String getRange(int start) {
-        return null;
+        String result = "";
+        int number = 0;
+        while (number < start) {
+            result += number;
+            result += 1;
+        }
+        return result;
     }
 
     public static String getRange(int start, int stop) {
+        String result = "";
+        int number = start;
+        while (number < stop) {
+            result += number;
+            result += 1;
+        }
         return null;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String result = "";
+        int number = start;
+        while (number < stop) {
+            result += number;
+            result += step;
+        }
+        return result;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String result = "";
+        int number = start;
+        while (number < stop) {
+            number += 1;
+            number = (int)Math.pow(number, exponent);
+        }
+        return result;
     }
 }
